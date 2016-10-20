66c4d4761457d3949ae6c00e8648a3c9a776bc8f
jpedrocm <jpcm@cin.ufpe.br>
2016-10-20 19:06:35 -0300

    Redid grammar creation and added metrics

--- parser.py
+++ parser.py
@@ -6,9 +6,9 @@ def create_sets():
 	train_set = []
 	test_set = []
 
-	full_raw_set = treebank.parsed_sents()
+	raw_full_set = treebank.parsed_sents()
 
-	full_set = map(lambda tree: binarize_tree(tree), full_raw_set)
+	full_set = map(lambda tree: binarize_tree(tree), raw_full_set)
 
 	full_set_size = len(full_set)
 	full_set_indexes = range(full_set_size)
@@ -25,23 +25,70 @@ def binarize_tree(tree):
 	tree.chomsky_normal_form()
 	return tree
 
-def extract_rules(sentences):
-	grammar_rules = []
+def extract_rules(trees):
+	rules = {}
+	for tree in trees:
+		tree_rules = tree.productions()
+		for rule in tree_rules:
+			left_side = rule.lhs()
+			right_side = rule.rhs()
+			if left_side not in rules:
+				rules[left_side] = {right_side: 1}
+			else:
+				if right_side not in rules[left_side]:
+					rules[left_side][right_side] = 1
+				else:
+					rules[left_side][right_side] += 1
+	return rules
 
-	for sentence in sentences:
-		grammar_rules += sentence.productions()
+def normalize_and_transform_rules(rules):
+	grammar = {}
+	for (left_side, right_sides_list) in rules.iteritems():
+		total_instances = float(sum(right_sides_list.values()))
+		for (right_side, num_of_instances) in right_sides_list.iteritems():
+			if right_side not in grammar:
+				grammar[right_side] = {left_side: (num_of_instances/total_instances)}
+			else:
+				if left_side not in grammar[right_side]:
+					grammar[right_side][left_side] = (num_of_instances/total_instances)
+				else:
+					print "ERROR"
+	return grammar
 
-	return grammar_rules
+def create_pcfg(trees):
+	raw_rules = extract_rules(trees)
+	grammar = normalize_and_transform_rules(raw_rules)
+	return grammar
 
-def create_pcfg(rules):
-	S = Nonterminal('S')
-	return induce_pcfg(S, rules)
+def calculate_metric_of_sentence(labeled, predicted):
+	num_of_same_label = len(filter(lambda x: x in labeled, predicted))
+
+	precision = num_of_same_label / float(len(predicted))
+	recall = num_of_same_label / float(len(labeled))
+	f1 = precision / recall
+	tagging_accuracy = 0 ############# TODO
+	return [precision, recall, f1, tagging_accuracy]
+
+def calculate_parser_metrics(list_of_sentence_metrics):
+	parser_metrics_sum = {}
+	num_of_sentences = len(List_of_sentence_metrics)
+
+	parser_metrics_sum = [sum(x) for x in zip(*list_of_sentence_metrics)]
+
+	parser_metrics = map(lambda x: x/num_of_sentences)
+
+	return parser_metrics
+
+def print_metrics(parser_metrics):
+	print "METRICS"
+	print "Precision: " + str(parser_metrics[0])
+	print "Recall: " + str(parser_metrics[1])
+	print "F-measure: " + str(parser_metrics[2])
+	print "Tagging accuracy: " + str(parser_metrics[3])
 
 def main():
 	train_set, test_set = create_sets()
 
-	grammar_rules = extract_rules(train_set)
-
-	grammar = create_pcfg(grammar_rules)
+	pcfg = create_pcfg(train_set)
 
 main()
